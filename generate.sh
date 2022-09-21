9580adf715062af5edd4619522fcb2817f6fa6cc
Nicholas Long <nicholas.eden.long@gmail.com>
2022-09-21 19:38:43 +0000

    remove

--- generate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#exploitdb/scripts/generate-exploitdb.sh > exploitdb/data/exploitdb_links_raw.tsv
-exploitdb/scripts/usable-raw-urls.awk exploitdb/data/url-results.tsv > exploitdb/data/usable-raw-urls.tsv
-exploitdb/scripts/remaining-urls.awk exploitdb/data/url-results.tsv > exploitdb/data/remaining-to-process.tsv
