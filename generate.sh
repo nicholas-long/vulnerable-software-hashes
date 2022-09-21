567250aa100554d28c7a8feece454e1515694f61
Nicholas Long <nicholas.eden.long@gmail.com>
2022-09-21 16:52:30 +0000

    get remaining URLs to process

--- generate.sh
+++ generate.sh
@@ -2,3 +2,4 @@
 
 #exploitdb/scripts/generate-exploitdb.sh > exploitdb/data/exploitdb_links_raw.tsv
 exploitdb/scripts/usable-raw-urls.awk exploitdb/data/url-results.tsv > exploitdb/data/usable-raw-urls.tsv
+exploitdb/scripts/remaining-urls.awk exploitdb/data/url-results.tsv > exploitdb/data/remaining-to-process.tsv
