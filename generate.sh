efa2470030cce10daeb14e23ed1ada0839efae35
Nicholas Long <nicholas.eden.long@gmail.com>
2022-09-21 16:41:17 +0000

    zip files

--- generate.sh
+++ generate.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-exploitdb/scripts/generate-exploitdb.sh > exploitdb/data/exploitdb_links_raw.tsv
+#exploitdb/scripts/generate-exploitdb.sh > exploitdb/data/exploitdb_links_raw.tsv
+exploitdb/scripts/usable-raw-urls.awk exploitdb/data/url-results.tsv > exploitdb/data/usable-raw-urls.tsv
